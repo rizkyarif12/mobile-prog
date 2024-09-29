c34c6de4eb8449257417c3a7f690e79c41795796
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-09-29 16:14:47 +0700

    Flutter Project Preparation

--- week-5/week_lima/bin/week_lima.dart
+++
@@ -1,5 +0,0 @@
-import 'package:week_lima/week_lima.dart' as week_lima;
-
-void main(List<String> arguments) {
-  print('Hello world: ${week_lima.calculate()}!');
-}
--- week-5/week_lima/test/week_lima_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:week_lima/week_lima.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
