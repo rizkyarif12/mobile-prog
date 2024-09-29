239756b9d15cdd3a54c2601045a758ed65318f1d
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-09-29 16:08:52 +0700

    dart-project

---
+++ week-5/week_lima/bin/week_lima.dart
@@ -0,0 +1,5 @@
+import 'package:week_lima/week_lima.dart' as week_lima;
+
+void main(List<String> arguments) {
+  print('Hello world: ${week_lima.calculate()}!');
+}
---
+++ week-5/week_lima/test/week_lima_test.dart
@@ -0,0 +1,8 @@
+import 'package:week_lima/week_lima.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
