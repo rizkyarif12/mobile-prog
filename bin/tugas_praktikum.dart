522075584282010cae9b0ee23fd1b2b96efc1903
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-09-29 15:43:17 +0700

    Merapihkan Folder

--- bin/tugas_praktikum.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:tugas_praktikum/tugas_praktikum.dart' as tugas_praktikum;
-
-void main() {
-  for (int i = 18; i >= 9; i--) {
-    print('Nama Saya Rizky, sekarang berumur $i');
-  }
-}
