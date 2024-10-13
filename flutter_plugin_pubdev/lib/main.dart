25a8033b18e00d59ce212f5bdea320244baa4dfa
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-10-13 18:50:00 +0700

    Langkah 7

--- flutter_plugin_pubdev/lib/main.dart
+++ flutter_plugin_pubdev/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pubdev/red_text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -105,6 +106,20 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              color: Colors.yellowAccent,
+              width: 50,
+              child: const RedTextWidget(
+                text: 'You have pushed the button this many times:',
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              width: 100,
+              child: const Text(
+                'You have pushed the button this many times:',
+              ),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
